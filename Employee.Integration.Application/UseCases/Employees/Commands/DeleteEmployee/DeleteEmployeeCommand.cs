f93c6f680944d8b5801012a8ea13c55a2b5fb8bf
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-08-31 17:46:38 +0500

    save

--- Employee.Integration.Application/UseCases/Employees/Commands/DeleteEmployee/DeleteEmployeeCommand.cs
+++ Employee.Integration.Application/UseCases/Employees/Commands/DeleteEmployee/DeleteEmployeeCommand.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Employee.Integration.Application.Common.Exceptions;
+using Employee.Integration.Application.Common.Interfaces;
+using MediatR;
 
 namespace Employee.Integration.Application.UseCases.Employees.Commands.DeleteEmployee
 {
-    internal class DeleteEmployeeCommand
+    public record DeleteEmployeeCommand(int Id) : IRequest;
+
+    public class DeleteEmployeeCommandHandler : IRequestHandler<DeleteEmployeeCommand>
     {
+        private IApplicationDbContext _dbContext;
+        private IMapper _mapper;
+
+        public DeleteEmployeeCommandHandler(IApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
+        {
+            Domain.Entities.Employee Employee = FilterIfEmployeeExsists(request.Id);
+
+            _dbContext.Employees.Remove(Employee);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        private Domain.Entities.Employee FilterIfEmployeeExsists(int id)
+            => _dbContext.Employees
+            .FirstOrDefault(c => c.Id == id)
+                ?? throw new NotFoundException(
+                    " There is no Employee with id. ");
+
     }
 }
--- Employee.Integration.Application/UseCases/Employees/Commands/UpdateEmployee/UpdateEmployeeCommand.cs
+++ Employee.Integration.Application/UseCases/Employees/Commands/UpdateEmployee/UpdateEmployeeCommand.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using DocumentFormat.OpenXml.Drawing.Charts;
+using Employee.Integration.Application.Common.Exceptions;
+using Employee.Integration.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Employee.Integration.Application.UseCases.Employees.Commands.UpdateEmployee
 {
-    internal class UpdateEmployeeCommand
+    public class UpdateEmployeeCommand:IRequest
     {
+        public int Id { get; set; }
+        public string Payroll_Number { get; set; }
+        public string Forenames { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Telephone { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public string Address_2 { get; set; }
+        public string Postcode { get; set; }
+        public string EMail_Home { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+
+    public class UpdateEmployeeCommandHandler : IRequestHandler<UpdateEmployeeCommand>
+    {
+        private readonly IMapper _mapper;
+        private readonly IApplicationDbContext _context;
+
+        public UpdateEmployeeCommandHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task Handle(UpdateEmployeeCommand request, CancellationToken cancellationToken)
+        {
+            Domain.Entities.Employee employee = await FilterIfEmployeeExists(request.Id);
+            _mapper.Map(request, employee);
+            _context.Employees.Update(employee);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        private async Task<Domain.Entities.Employee> FilterIfEmployeeExists(int id)
+         => await _context.Employees
+                    .FirstOrDefaultAsync(x => x.Id == id)
+                     ?? throw new NotFoundException(
+                              " there is no employee with this id. ");
     }
 }
