536857a7a144ab078e05977755497d0865ab2ed1
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-08-31 17:11:54 +0500

    Validation Added

--- Employee.Integration.Application/UseCases/Employees/Commands/CreateEmployee/CreateEmployeeCommandValidator.cs
+++ Employee.Integration.Application/UseCases/Employees/Commands/CreateEmployee/CreateEmployeeCommandValidator.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 
 namespace Employee.Integration.Application.UseCases.Employees.Commands.CreateEmployee
 {
-    internal class CreateEmployeeCommandValidator
+    public class CreateEmployeeCommandValidator : AbstractValidator<CreateEmployeeCommand>
     {
+        public CreateEmployeeCommandValidator()
+        {
+            RuleFor(d => d.Payroll_Number)
+                .NotEmpty()
+                .MaximumLength(100)
+                .WithMessage("Payroll_Number is required");
+
+            RuleFor(d => d.Forenames)
+               .NotEmpty()
+               .MaximumLength(100)
+               .WithMessage("Forenames is required");
+
+            RuleFor(d => d.Surname)
+               .NotEmpty()
+               .MaximumLength(100)
+               .WithMessage("Surname is required");
+
+            RuleFor(t => t.DateOfBirth)
+                .NotNull()
+                .WithMessage("Date Of Birth date is required.");
+
+            RuleFor(d => d.Telephone)
+               .NotEmpty()
+               .MaximumLength(100)
+               .WithMessage("Telephone Number is required");
+
+            RuleFor(d => d.Mobile)
+               .NotEmpty()
+               .MaximumLength(100)
+               .WithMessage("Mobile Number is required");
+
+            RuleFor(d => d.Address)
+               .NotEmpty()
+               .MaximumLength(100)
+               .WithMessage("Address is required");
+
+            RuleFor(d => d.Address_2)
+               .NotEmpty()
+               .MaximumLength(100)
+               .WithMessage("Address_2 is required");
+
+            RuleFor(d => d.Postcode)
+                .NotEmpty()
+                .MaximumLength(100)
+                .WithMessage("Postcode is required");
+
+            RuleFor(d => d.EMail_Home)
+                .NotEmpty()
+                .MaximumLength(250)
+                .WithMessage("Home email required");
+
+            RuleFor(t => t.StartDate)
+                .NotNull()
+                .WithMessage("Start Date date is required.");
+        }
     }
 }
