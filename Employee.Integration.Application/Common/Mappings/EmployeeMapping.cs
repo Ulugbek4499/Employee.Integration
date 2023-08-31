c5676a3b03d2947c717c5f0879a23d500cb7072f
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-08-31 17:04:33 +0500

    save

--- Employee.Integration.Application/Common/Mappings/EmployeeMapping.cs
+++ Employee.Integration.Application/Common/Mappings/EmployeeMapping.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace Employee.Integration.Application.Common.Mappings
 {
-    public class EmployeeMapping
+    public class EmployeeMapping : Profile
     {
-
+        public EmployeeMapping()
+        {
+         /*   CreateMap<,>().ReverseMap();
+            CreateMap<,>().ReverseMap();
+            CreateMap<,>().ReverseMap();
+            CreateMap<,>().ReverseMap();*/
+        }
     }
 }
--- Employee.Integration.Application/UseCases/Employees/Commands/CreateEmployee/CreateEmployeeCommand.cs
+++ Employee.Integration.Application/UseCases/Employees/Commands/CreateEmployee/CreateEmployeeCommand.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Employee.Integration.Application.Common.Interfaces;
+using MediatR;
 
 namespace Employee.Integration.Application.UseCases.Employees.Commands.CreateEmployee
 {
-    internal class CreateEmployeeCommand
+    public class CreateEmployeeCommand : IRequest<int>
     {
+        public string Payroll_Number { get; set; }
+        public string Forenames { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Telephone { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public string Address_2 { get; set; }
+        public string Postcode { get; set; }
+        public string EMail_Home { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+    public class CreateEmployeeCommandHandler : IRequestHandler<CreateEmployeeCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly IApplicationDbContext _context;
+
+        public CreateEmployeeCommandHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateEmployeeCommand request, CancellationToken cancellationToken)
+        {
+            Domain.Entities.Employee employee = _mapper.Map<Domain.Entities.Employee>(request);
+            await _context.Employees.AddAsync(employee, cancellationToken);
+            await _context.SaveChangesAsync();
+
+            return employee.Id;
+        }
     }
 }
