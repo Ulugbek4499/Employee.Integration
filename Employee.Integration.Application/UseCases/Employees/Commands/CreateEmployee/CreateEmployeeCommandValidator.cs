0692ef7ae6df62e3a5ecd797ddd95a73d796295c
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-08-31 17:12:14 +0500

    Validation Added

--- Employee.Integration.Application/UseCases/Employees/Commands/CreateEmployee/CreateEmployeeCommandValidator.cs
+++ Employee.Integration.Application/UseCases/Employees/Commands/CreateEmployee/CreateEmployeeCommandValidator.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentValidation;
+﻿using FluentValidation;
 
 namespace Employee.Integration.Application.UseCases.Employees.Commands.CreateEmployee
 {
