dced8d26cd936cc5965bfe82ff07dae7d243aaa8
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-09-04 08:35:04 +0500

    save

--- Employee.Integration.MVC/Controllers/ApiBaseController.cs
+++ Employee.Integration.MVC/Controllers/ApiBaseController.cs
@@ -9,8 +9,5 @@ namespace Employee.Integration.MVC.Controllers
     {
         private IMediator? _mediator;
         public IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>();
-
-        /*        protected IWebHostEnvironment _hostEnviroment
-               => HttpContext.RequestServices.GetRequiredService<IWebHostEnvironment>();*/
     }
 }
